8b8b7e481f05f1cfb39b2ba54694a1820732267c
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-02 22:22:13 +0900

    add: no vowels app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
-import { Box, Link } from '@chakra-ui/react'
+import { Box, Container, Heading, Link, ListItem, UnorderedList } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <Box>
-      <Link href="/tetris">Tetris</Link>
-    </Box>
+    <>
+      <Head>
+        <title>SHOWROOM</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container sx={{ py: "3rem" }}>
+        <Heading as="h2" sx={{ textAlign: "center" }}>SHOWROOM</Heading>
+        <UnorderedList>
+          <ListItem><Link href="/tetris">Tetris</Link></ListItem>
+          <ListItem><Link href="/no_vowels">No Vowels</Link></ListItem>
+        </UnorderedList>
+      </Container>
+    </>
   )
 }
 
--- src/pages/no_vowels.tsx
+++ src/pages/no_vowels.tsx
@@ -1,12 +1,46 @@
+import { ChangeEvent, useState } from 'react'
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
-import { Box } from '@chakra-ui/react'
+import { Box, Container, Heading, Input, Text } from '@chakra-ui/react'
 
 const NoVowels: NextPage = () => {
-  return (
-    <Box>
+  const [text, setText] = useState("");
 
-    </Box>
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>No Vowels</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container sx={{ py: "3rem" }}>
+        <Heading as="h2" sx={{ textAlign: "center" }}>No Vowels</Heading>
+        <Input
+          placeholder='Type something...'
+          value={text}
+          onChange={handleChange}
+          sx={{ my: "1rem" }}
+        />
+        <Heading as="h3" size="lg" sx={{ textAlign: "center" }}>Result</Heading>
+        {text && (
+          <Box borderWidth={1} borderRadius="lg" sx={{ my: "1rem", p: "1rem" }}>
+            <Text fontSize="xl" sx={{ letterSpacing: ".5rem" }}>
+              {Array.from(text).map(s => {
+                if (['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'].includes(s)) {
+                  return '_'
+                } else {
+                  return s
+                }
+              }).join("")}
+            </Text>
+          </Box>
+        )}
+      </Container>
+    </>
   )
 }
 
